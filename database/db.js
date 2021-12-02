7db3e9c7879373ae12d923146706335c3e8ab26a
Katashihana <89012832+Katashihana@users.noreply.github.com>
2021-12-03 01:21:13 +0700

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ monk = require('monk'),
 { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-let url = 'https://docs-katashi.herokuapp.com';
+let url = 'https://api-velgrynd.herokuapp.com';
 try {
-if(url == 'https://docs-katashi.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api-velgrynd.herokuapp.com') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 return;
 }
